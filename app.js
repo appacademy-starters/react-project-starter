a3c0d8724c4f0fbbc3adfc649bdfe428b63419dc
Ian Bentley <ian@idbentley.com>
2020-08-27 12:05:19 -0400

    test negative api :wq match

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(routes);
 // This should come after routes, but before 404 and error handling.
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/", (req, res) => {
+  app.get(/\/(?!api)*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
